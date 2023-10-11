2984a0e1d10b2ab5ae4ac7b7502c720420e82942
deepali-02 <deepali93.redekar@gmail.com>
2023-10-11 14:41:58 +0200

    added custom footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Component {...pageProps} />
+    <footer><p>@deepali 2023</p> </footer>
+    </div>
 }
