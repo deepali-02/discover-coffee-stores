7f9e493cd69299fba027f05963b5e35b66d4556a
deepali-02 <deepali93.redekar@gmail.com>
2023-10-13 13:32:27 +0200

    added non dynamic link to go back to home using Link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log("router", router);
-  return <div>Coffee stores Page {router.query.id}</div>;
+  return <div>Coffee stores Page {router.query.id}
+  <Link href="/">Back to Home</Link>
+  </div>;
 };
 
 export default CoffeeStore;
