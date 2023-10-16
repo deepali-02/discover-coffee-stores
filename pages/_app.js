41957012e5c975d7becd3836760f14ada0342f4b
deepali-02 <deepali93.redekar@gmail.com>
2023-10-16 13:44:33 +0200

    removed footer

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,5 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <div>
     <Component {...pageProps} />
-    <footer><p>@deepali 2023</p> </footer>
     </div>
 }
